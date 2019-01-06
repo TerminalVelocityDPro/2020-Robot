ff2cae18499eb76af08bf9c2d59dd752945b0b51
Deepro Pasha <fkpas@mpasha-e5440.ad.csufresno.edu>
2019-01-06 15:49:21 -0800

    updating tank drive system

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,6 +7,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -39,4 +43,24 @@ public class OI {
   // Start the command when the button is released and let it run the command
   // until it is finished as determined by it's isFinished method.
   // button.whenReleased(new ExampleCommand());
+  public static Joystick leftJoystick = new Joystick(0);
+  public static Joystick rightJoystick = new Joystick(1);
+
+  public static JoystickButton leftTrigger = new JoystickButton(leftJoystick, 1);
+	public static JoystickButton leftButton3 = new JoystickButton(leftJoystick, 3);
+	public static JoystickButton leftButton4 = new JoystickButton(leftJoystick, 4);
+	public static JoystickButton leftButton5 = new JoystickButton(leftJoystick, 5);
+	public static JoystickButton leftButton6 = new JoystickButton(leftJoystick, 6);
+
+	
+
+	public static JoystickButton rightTrigger = new JoystickButton(rightJoystick, 1);
+	public static JoystickButton rightButton3 = new JoystickButton(rightJoystick, 3);
+  public static JoystickButton rightButton4 = new JoystickButton(rightJoystick, 4);
+  
+  public static XboxController xbox = new XboxController(2);
+
+  static{
+  
+  }
 }

07173a3947cdbc5eded6301342f9159765a6e273
Drive Station 2 <barnapa@yahoo.com>
2019-02-16 12:29:05 -0800

    Importing in auto record code

---
+++ src/main/java/frc/robot/MacroPlayRecord/BTMacroPlay.java
@@ -0,0 +1,95 @@
+package frc.robot.MacroPlayRecord;
+
+
+
+import java.io.File;
+
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+
+
+
+import frc.robot.subsystems.DriveTrain;
+
+
+
+public class BTMacroPlay{
+
+
+
+    DriveTrain driveTrain = DriveTrain.getInstance();
+
+    Scanner scanner;
+
+    long startTime;
+
+
+
+    boolean onTime = true;
+
+    double nextDouble;
+
+
+
+    public BTMacroPlay() throws FileNotFoundException{
+
+        scanner = new Scanner(new File(""));
+
+
+
+        scanner.useDelimiter(",|\\n");
+
+
+
+        startTime = System.currentTimeMillis();
+
+    }
+
+
+
+    public void play(){
+
+        if((scanner != null) && (scanner.hasNextDouble())){
+
+            double t_delta;
+
+
+
+            if(onTime){
+
+                nextDouble = scanner.nextDouble();
+
+            }
+
+
+
+            t_delta = nextDouble - (System.currentTimeMillis()-startTime);
+
+
+
+            if(t_delta <= 0){
+
+                driveTrain.setFrontLeft(scanner.nextDouble());
+
+                driveTrain.setFrontRight(scanner.nextDouble());
+
+                driveTrain.setBackLeft(scanner.nextDouble());
+
+                driveTrain.setBackRight(scanner.nextDouble());
+
+
+
+                onTime  = true;
+
+
+
+
+
+            }
+
+        }
+
+    }
+
+}
---
+++ src/main/java/frc/robot/MacroPlayRecord/BTMacroRecord.java
@@ -0,0 +1,89 @@
+package frc.robot.MacroPlayRecord;
+
+
+
+import java.io.FileWriter;
+
+import java.io.IOException;
+
+
+
+import frc.robot.subsystems.DriveTrain;
+
+
+
+public class BTMacroRecord{
+
+
+
+    DriveTrain driveTrain = DriveTrain.getInstance();
+
+
+
+    FileWriter writer;
+
+
+
+    long startTime;
+
+
+
+    public BTMacroRecord() throws IOException{
+
+
+
+        startTime = System.currentTimeMillis();
+
+
+
+        //make sure to specify file location
+
+        writer = new FileWriter("");
+
+
+
+    }
+
+
+
+    public void record() throws IOException{
+
+        if(writer != null){
+
+            writer.append("" + (System.currentTimeMillis()-startTime));
+
+            writer.append("," + driveTrain.getBackLeft());
+
+            writer.append("," + driveTrain.getFrontRight());
+
+            writer.append("," + driveTrain.getBackLeft());
+
+            writer.append("," + driveTrain.getBackRight());
+
+        }
+
+    }
+
+
+
+    public void end() throws IOException{
+
+        if(writer!=null){
+
+            writer.flush();
+
+            writer.close();
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
